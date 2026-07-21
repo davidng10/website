497b7e81fb1006db78b0c7a26a7c2049d8f45bf5
Wen Bin <wenbin_ng@yahoo.com.sg>
2026-07-21 22:14:35 +0800

    chore: post lang fixes and added json ld

---
+++ src/data/author.ts
@@ -0,0 +1,13 @@
+export const AUTHOR = {
+  "@type": "Person",
+  "@id": "/about/#person",
+  name: "Ng Wen Bin",
+  alternateName: ["Wen Bin", "David"],
+  url: "/about/",
+  image: "/profile_pic.webp",
+  jobTitle: "Software Engineer",
+  sameAs: [
+    "https://www.linkedin.com/in/ngwenbin/",
+    "https://github.com/davidng10/",
+  ],
+} as const;
